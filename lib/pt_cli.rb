1ad5193222342866164291b9750f0f92c0337143
Roderick Bompart <rbompart9@gmail.com>
2023-03-02 02:03:35 -0400

    chore: :tada: Add initial files

---
+++ lib/pt_cli.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "pt_cli/version"
+
+module PtCli
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/pt_cli/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module PtCli
+  VERSION = "0.1.0"
+end
